f941bde1f3fb878c888147fc7cf99ade4bbaccd2
Victor <VictorN076@gmail.com>
2023-09-24 16:54:05 -0500

    still stripping

--- config.h
+++ config.h
@@ -1,33 +1,3 @@
 #pragma once
 
-/*
-https://docs.qmk.fm/#/squeezing_avr?id=configh-settings
-https://docs.qmk.fm/#/config_options?id=hardware-options
-*/
-
-#include "config\tapping_config.h"
-
-//Turn off settings we won't use to save memory
-#undef LOCKING_SUPPORT_ENABLE
-#undef LOCKING_RESYNC_ENABLE
-
-//Clear variables we plan to use that might be set elsewhere
-#undef USB_POLLING_INTERVAL_MS
-#undef SPLIT_TRANSPORT_MIRROR
-#undef SPLIT_LAYER_STATE_ENABLE
-#undef SPLIT_LED_STATE_ENABLE
-#undef SPLIT_MODS_ENABLE
-#undef SPLIT_USB_DETECT
-
-//Set common configuration for all keyboards
-#define USB_POLLING_INTERVAL_MS 1
-
-#if defined(SPLIT_KEYBOARD)
-#   define SPLIT_TRANSPORT_MIRROR
-#   define SPLIT_LAYER_STATE_ENABLE
-#   define SPLIT_LED_STATE_ENABLE
-#   define SPLIT_MODS_ENABLE
-//#   define SPLIT_USB_DETECT
-#endif //SPLIT_KEYBOARD
-
 #include "layouts.h"
