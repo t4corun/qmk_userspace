632aa1af7071d1c2eac7418b4fe8ba9b5fb85c49
Victor <VictorN076@gmail.com>
2023-09-23 23:25:48 -0500

    Added wrapper thing - was getting error passing in layers to the wrapper - added the line calsoylar

--- T4CORUN.h
+++ T4CORUN.h
@@ -282,6 +282,7 @@ I prefer to use KC_NO over KC_TRNS so I don't have accidental presses.
 #   define         _________TRANS_R4________                  ___x___, _______, ___x___
 #endif //KEYBOARD_bastardkb_charybdis_3x5
 
+#define LAYOUT_wrapper(...) LAYOUT_charybdis_3x5(__VA_ARGS__)
 
 
 /*
