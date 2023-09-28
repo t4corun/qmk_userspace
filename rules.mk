45831fda2e8dbffd6e2c099ab232fe0fe8f86887
Victor <VictorN076@gmail.com>
2023-09-27 21:51:58 -0500

    addressing warnings - added a newline after the basetest wrapper - removing more of the rules.mk

--- rules.mk
+++ rules.mk
@@ -1,13 +1,2 @@
 SRC += \
 	t4corun.c
-
-# default settings across all keyboards
-# ?= means set the value if its not defined elsewhere
-# writen this way so we can use this in a local build environment 
-# build firmware for other keyboards
-SPACE_CADET_ENABLE ?= no
-GRAVE_ESC_ENABLE ?= no
-DYNAMIC_MACRO_ENABLE ?= yes
-LTO_ENABLE ?= yes
-MAGIC_ENABLE ?= no
-REPEAT_KEY_ENABLE ?= yes
