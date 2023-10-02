3336c858bb15ae43ad396db62dfb46a0d8eb0e14
Victor <VictorN076@gmail.com>
2023-10-01 22:01:04 -0500

    adding stuff back

--- _layouts.h
+++ _layouts.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#define C_35(k) L3x6_to_CharybdisNano(k)
 #define L3x6_to_CharybdisNano( \
 	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
 	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
