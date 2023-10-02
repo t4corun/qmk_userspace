07069d04de3b8d282db965c8a4761e612db7129a
Victor <VictorN076@gmail.com>
2023-10-01 22:59:58 -0500

    cleaning up some syntax

--- config/split_config.h
+++ config/split_config.h
@@ -11,8 +11,9 @@ https://docs.qmk.fm/#/feature_split_keyboard
 #undef SPLIT_MODS_ENABLE
 #undef SPLIT_USB_DETECT
 
-#   define SPLIT_TRANSPORT_MIRROR
-#   define SPLIT_LAYER_STATE_ENABLE
-#   define SPLIT_LED_STATE_ENABLE
-#   define SPLIT_MODS_ENABLE
+
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
 //#   define SPLIT_USB_DETECT
