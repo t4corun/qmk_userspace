a1b6ff34acf7011d324034343342b27888c0bc15
Victor <VictorN076@gmail.com>
2023-10-01 21:12:13 -0500

    adding my custom keycodes only

--- _keycodes.h
+++ _keycodes.h
@@ -19,3 +19,158 @@ enum keycodes {
 };
 
 #define ___x___ KC_NO
+
+//momentary layer
+#define NAV        MO(_NAVIGATION)
+#define SYM        MO(_SYMBOL)
+#define CONFIG     MO(_CONFIG)
+#define GAMENUM    MO(_GAMENUMBER)
+
+//Windows Shortcuts
+#define SC_COPY    LCTL(KC_C)
+#define SC_CUT     LCTL(KC_X)
+#define SC_UNDO    LCTL(KC_Z)
+#define SC_PAST    LCTL(KC_V)
+#define SC_REDO    LCTL(KC_Y)
+
+
+/*
+- Any keycodes with prefix TR has behavior dependent on if the feature is enabled.
+- If the feature is disabled then the key is just the normal key
+- We will only define things that are used across different layers. Entire layers are turned off in the keymap
+*/
+
+#if defined(ONESHOT_ENABLE)
+#   define TR_LSFT OSM(MOD_LSFT)
+#   define TR_LCTL OSM(MOD_LCTL)
+#   define TR_LALT OSM(MOD_LALT)
+#   define TR_LGUI OSM(MOD_LGUI)
+#else
+#   define TR_LSFT KC_LSFT
+#   define TR_LCTL KC_LCTL
+#   define TR_LALT KC_LALT
+#   define TR_LGUI KC_LGUI
+#endif //ONESHOT_ENABLE
+
+#if defined(MOUSELAYER_ENABLE)
+#   define MOUSE   MO(_MOUSE)
+#   define TR_BTN1 KC_BTN1
+#   define TR_BTN2 KC_BTN2
+#   define TR_BTN3 KC_BTN3
+#   define TR_BTN4 KC_BTN4
+#   define TR_BTN5 KC_BTN5
+#else
+#   define MOUSE   ___x___
+#   define TR_BTN1 ___x___
+#   define TR_BTN2 ___x___
+#   define TR_BTN3 ___x___
+#   define TR_BTN4 ___x___
+#   define TR_BTN5 ___x___
+#endif //MOUSELAYER_ENABLE
+
+#if defined(MOUSEKEY_ENABLE)
+#   define TR_MOUU KC_MS_U
+#   define TR_MOUD KC_MS_D
+#   define TR_MOUL KC_MS_L
+#   define TR_MOUR KC_MS_R
+#   define TR_MWHU KC_WH_U
+#   define TR_MWHD KC_WH_D
+#else
+#   define TR_MOUU ___x___
+#   define TR_MOUD ___x___
+#   define TR_MOUL ___x___
+#   define TR_MOUR ___x___
+#   define TR_MWHU ___x___
+#   define TR_MWHD ___x___
+#endif //MOUSEKEY_ENABLE
+
+//these keycodes come from the cnano
+#if defined(POINTING_DEVICE_ENABLE)
+#   define TR_SNIP SNIPING
+#   define TR_SNIT SNP_TOG
+#   define TR_DRGS DRGSCRL
+#   define TR_DRGT DRG_TOG
+#   define TR_SDPI S_D_MOD
+#   define TR_PDPI DPI_MOD
+#else
+#   define TR_SNIP ___x___
+#   define TR_SNIT ___x___
+#   define TR_DRGS ___x___
+#   define TR_DRGT ___x___
+#   define TR_SDPI ___x___
+#   define TR_PDPI ___x___
+#endif //POINTING_DEVICE_ENABLE
+
+#if defined(GAMELAYER_ENABLE) 
+#   define TR_GAME GAMING
+#else
+#   define TR_GAME ___x___
+#endif //GAMELAYER_ENABLE
+
+// the behavior is handled in process_tap_hold_key. 
+// the important thing is that we define a layer taps
+#if defined(TAPHOLD_ENABLE)
+//Brackets
+#   define TR_LBRC  LT(_DEFAULT_LAYER_1, TH_LBRC)
+#   define TR_LABK  LT(_DEFAULT_LAYER_1, TH_LABK)
+//Custom
+#   define TR_LPRN  LT(_DEFAULT_LAYER_1, TH_LPRN)
+#   define TR_PERC  LT(_DEFAULT_LAYER_1, TH_PERC)
+#   define TR_COMM  LT(_DEFAULT_LAYER_1, TH_COMM)
+#   define TR_DOT   LT(_DEFAULT_LAYER_1, TH_DOT)
+#else
+//Brackets
+#   define TR_LBRC  KC_LBRC
+#   define TR_LABK  KC_LABK
+//Custom
+#   define TR_LPRN  KC_LPRN
+#   define TR_PERC  KC_PERC //TODO when tap hold is off typing ^ will not be possible
+#   define TR_COMM  KC_COMM
+#   define TR_DOT   KC_DOT
+#endif //TAPHOLD_ENABLE
+
+#if defined(DYNAMIC_MACRO_ENABLE)
+#   define TR_DMR1 DM_REC1
+#   define TR_DMP1 DM_PLY1
+#   define TR_DMR2 DM_REC2
+#   define TR_DMP2 DM_PLY2
+#else
+#   define TR_DMR1 ___x___
+#   define TR_DMP1 ___x___
+#   define TR_DMR2 ___x___
+#   define TR_DMP2 ___x___
+#endif //DYNAMIC_MACRO_ENABLE
+
+#if defined(RGB_MATRIX_ENABLE) || defined(RGBLIGHT_ENABLE)
+#   define TR_RHUI RGB_HUI
+#   define TR_RSAI RGB_SAI
+#   define TR_RVAI RGB_VAI
+#   define TR_RSPI RGB_SPI
+#   define TR_RTOG RGB_TOG
+#   define TR_RMOD RGB_MOD
+#else
+#   define TR_RHUI ___x___
+#   define TR_RSAI ___x___
+#   define TR_RVAI ___x___
+#   define TR_RSPI ___x___
+#   define TR_RTOG ___x___
+#   define TR_RMOD ___x___
+#endif //RGB_MATRIX_ENABLE or RGBLIGHT_ENABLE
+
+#if defined(REPEAT_KEY_ENABLE)
+#   define TR_REP QK_REP
+#else
+#   define TR_REP TR_LSFT
+#endif //REPEAT_KEY_ENABLE
+
+#if defined(AUTO_SHIFT_ENABLE)
+#   define TR_ASDN AS_DOWN
+#   define TR_ASUP AS_UP
+#   define TR_ASRP AS_RPT
+#   define TR_ASTG AS_TOGG
+#else
+#   define TR_ASDN ___x___
+#   define TR_ASUP ___x___
+#   define TR_ASRP ___x___
+#   define TR_ASTG ___x___
+#endif //REPEAT_KEY_ENABLE
--- t4corun.h
+++ t4corun.h
@@ -1,4 +1,2 @@
 #pragma once
 #include QMK_KEYBOARD_H
-
-#include "_keycodes.h"
