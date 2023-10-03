a3d7a4010b3daee5bd390afa5699167d41e3c1ae
Victor <VictorN076@gmail.com>
2023-10-02 23:00:07 -0500

    cleaning up songs and putting const

--- features/audio.h
+++ features/audio.h
@@ -1,13 +1,10 @@
 #pragma once
 #include "t4corun.h"
 
-float plover_song[][2]     	= SONG(PLOVER_SOUND);
-float plover_gb_song[][2]  	= SONG(PLOVER_GOODBYE_SOUND);
-float lower_song[][2]  		= SONG(CAPS_LOCK_ON_SOUND);
-float raise_song[][2]  		= SONG(NUM_LOCK_ON_SOUND);
-float fn_song[][2]  		= SONG(SCROLL_LOCK_ON_SOUND);
-float adjust_song[][2]  	= SONG(UNICODE_WINDOWS);
-float default_sound[][2]  	= SONG(AG_NORM_SOUND);
+extern const float lower_song[][2]  		= SONG(CAPS_LOCK_ON_SOUND);
+extern const float default_sound[][2]  	= SONG(AG_NORM_SOUND);
+extern const float fn_song[][2]  		    = SONG(SCROLL_LOCK_ON_SOUND);
+extern const float adjust_song[][2]  	  = SONG(UNICODE_WINDOWS);
 
 layer_state_t layer_state_set_audio(layer_state_t state);
 void matrix_scan_muse(void);
