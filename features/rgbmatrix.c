19f8af0ef8bf44b84a7b14bee2c39e11f3467ac6
Victor <VictorN076@gmail.com>
2023-10-04 23:22:47 -0500

    forgot to put in the backled defines

--- features/rgbmatrix.c
+++ features/rgbmatrix.c
@@ -75,7 +75,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max){
   }
 
   if ( current_color.r != 0 || current_color.g != 0 || current_color.b != 0 ) {
-    for (uint8_t i = led_min; i < led_max; i++) {
+    for (uint8_t i = BACKLED_MIN; i < BACKLED_MAX; i++) {
       RGB_MATRIX_INDICATOR_SET_COLOR_wrapper( 
         i, 
         current_color.r,
