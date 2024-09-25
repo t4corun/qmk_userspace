f068cd914d7d149bfe58141307687cc952897d70
Victor <VictorN076@gmail.com>
2024-09-25 10:22:09 -0500

    Config changes and cleanup - Moved some of my keymap pointer config to the main keyboard as it should apply there too - added copyright information per QMK PR checklist - formatted JSON file per qmk format-json

--- keyboards/keyball/keyball39/keymaps/t4corun/config.h
+++ keyboards/keyball/keyball39/keymaps/t4corun/config.h
@@ -1,9 +1,20 @@
-#pragma once
+/* Copyright 2024 t4corun (@t4corun)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// These two configuration changes allow intuitive pointer movement
-// given the sensor placement
-#define ROTATIONAL_TRANSFORM_ANGLE -100
-#define POINTING_DEVICE_INVERT_X
+#pragma once
 
 // mimic mouse wheel direction
 #define KEYBALL_DRAGSCROLL_REVERSE_Y
