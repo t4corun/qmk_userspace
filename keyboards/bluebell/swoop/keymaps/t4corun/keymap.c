b00bf3c3511a8c929d32a05f4e62c6ce6b75ce57
Victor <VictorN076@gmail.com>
2024-10-06 21:17:10 -0500

    Reworked keymap wrappers for clarity - Changed the translation wrapper name because 3x5x3 doesn't fit anymore - Made the last alias KEYBOARD so it is easier to copy and paste between boards - Added documentation on how this works - updated cnano keymap so sniper layer activates with NAV layer

--- keyboards/bluebell/swoop/keymaps/t4corun/keymap.c
+++ keyboards/bluebell/swoop/keymaps/t4corun/keymap.c
@@ -16,9 +16,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "t4corun.h"
 
+/*
+ * Keymap pattern:
+ *
+ * - Choose a layout from the keyboard defintions (e.g. keyboard.json) and create an alias
+ * - Define which keys from my layers will be used/overriden in the target keyboard
+ *     For example: e01 and e02 represent encoder clicks. Those won't be passed to keybards
+ *     without encoders
+ * - Create an alias for the above translation
+ * - Create the keymaps array. Feed in my t4corun.h layers wrappers into the translation alias
+ */
+
 #define LAYOUT_swoop_wrapper(...) LAYOUT_split_3x5_3(__VA_ARGS__)
 
-#define LAYOUT_3x5_3_keymap(                                 \
+#define LAYOUT_t4corun_keymap(                               \
          k00, k01, k02, k03, k04,   k05, k06, k07, k08, k09, \
          k10, k11, k12, k13, k14,   k15, k16, k17, k18, k19, \
          k20, k21, k22, k23, k24,   k25, k26, k27, k28, k29, \
@@ -32,17 +43,17 @@ LAYOUT_swoop_wrapper (                                       \
               k32, k33, k34,   k35, k36, e02                 \
 )
 
-#define SWOOP(...) LAYOUT_3x5_3_keymap(__VA_ARGS__)
+#define KEYBOARD(...) LAYOUT_t4corun_keymap(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY]     = SWOOP(LAYER_QWERTY),
-  [_COLEMAK_DH] = SWOOP(LAYER_COLEMAK_DH),
-  [_GAME]       = SWOOP(LAYER_GAME),
-  [_NAVIGATION] = SWOOP(LAYER_NAVIGATION),
-  [_NUMBER]     = SWOOP(LAYER_NUMBER),
-  [_SYMBOL]     = SWOOP(LAYER_SYMBOL),
-  [_MOUSE]      = SWOOP(LAYER_MOUSE),
-  [_FUNCTION]   = SWOOP(LAYER_FUNCTION)
+  [_QWERTY]     = KEYBOARD(LAYER_QWERTY),
+  [_COLEMAK_DH] = KEYBOARD(LAYER_COLEMAK_DH),
+  [_GAME]       = KEYBOARD(LAYER_GAME),
+  [_NAVIGATION] = KEYBOARD(LAYER_NAVIGATION),
+  [_NUMBER]     = KEYBOARD(LAYER_NUMBER),
+  [_SYMBOL]     = KEYBOARD(LAYER_SYMBOL),
+  [_MOUSE]      = KEYBOARD(LAYER_MOUSE),
+  [_FUNCTION]   = KEYBOARD(LAYER_FUNCTION)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
