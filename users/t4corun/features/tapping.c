22e01413c747a07025b524399423ae8adec61767
t4corun <VictorN076@gmail.com>
2025-10-27 21:18:08 -0500

    Tweaking tapping stuff to prevent accidental HRM misfires - Turned off choral hold for GUI and ALT for the most part - Slowed down tapping term for GUI and ALT

--- users/t4corun/features/tapping.c
+++ users/t4corun/features/tapping.c
@@ -4,8 +4,23 @@ bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
                       uint16_t other_keycode, keyrecord_t* other_record) {
 
     switch (tap_hold_keycode) {
-        case HRM_A:
+        //Stop chordal from working for GUI and ALT
+        case HRM_A:  // Qwerty GUI
+            if (other_keycode == KC_SPC) {
+                return true;        
+            } else {
+                return false;
+            }
+            break;
+        case HRM_S:  // Qwerty Alt
+        case HRM_L:  // Qwerty Alt
+        case HRM_QT: // Qwerty GUI
+        case HRM_R:  // ColemakDH Alt
+        case HRM_I:  // ColemakDH Alt
+        case HRM_O:  // ColemakDH GUI
             return false;
+        default:
+            break;
     }
     // Otherwise defer to the opposite hands rule.
     return get_chordal_hold_default(tap_hold_record, other_record);
@@ -15,10 +30,15 @@ bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
     switch (keycode) {
-        case HRM_A:
-        case HRM_QT:
-        case HRM_O:
-            return TAPPING_TERM + 400;
+        // make GUI and ALT slower
+        case HRM_A:  // Qwerty GUI
+        case HRM_S:  // Qwerty Alt
+        case HRM_L:  // Qwerty Alt
+        case HRM_QT: // Qwerty GUI
+        case HRM_R:  // ColemakDH Alt
+        case HRM_I:  // ColemakDH Alt
+        case HRM_O:  // ColemakDH GUI
+            return TAPPING_TERM + 100;
         default:
             return TAPPING_TERM;
     }
